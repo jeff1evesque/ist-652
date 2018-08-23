ef159d6865f1cd6739d10ec3f189775ea3155eed
jeffrey levesque <jeff1evesque@yahoo.com>
2018-08-22 22:57:54 -0400

    #3: run.py, add file

---
+++ run.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import utility.twitter_scraper
+
+def run(twitter=True):
+    '''
+
+    execute custom twitter_scraper.
+
+    '''
+
+    tags = [
+        'apple',
+        'walmart',
+        'amazon',
+        'democrats',
+        'gop',
+        'nfl',
+        'nba',
+        'mlb',
+        'nhl',
+        'shakira',
+        'eminem',
+        'rihanna',
+        'justin_bieber',
+        'vin_diesel',
+        'will_smith',
+        'dwayne_johnson',
+        'jason_stratham',
+    ]
+
+    if twitter:
+        for tag in tags:
+            twitter_scraper(
+                tag,
+                outfile='data/{}--{s}.json'.format(
+                    datetime.now().strftime('%Y-%m-%d--%H-%M-%S'),
+                    tag
+                )
+            )
+
+if __name__ == '__main__':
+    run()
