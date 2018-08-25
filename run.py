3e0acfb2daca1fbf260e2cd24b1f92ef292de185
jeffrey levesque <jeff1evesque@yahoo.com>
2018-08-25 17:12:27 -0400

    #13: run.py, add facebook logic

--- run.py
+++ run.py
@@ -3,15 +3,21 @@
 import os
 from datetime import datetime
 from utility.twitter_scraper import twitter_scraper
+import fb_scrape_public as fsp
+import config
 
-def run(twitter=True):
+def run(twitter=True, facebook=True):
     '''
 
     execute custom twitter_scraper.
 
     '''
 
-    twitter_dir = 'data/twitter'
+    base_dir = 'data'
+    fb_token = config.token
+    twitter_dir = '{}/twitter'.format(base_dir)
+    facebook_dir = '{}/facebook'.format(base_dir)
+
     tags = [
         'apple',
         'walmart',
@@ -45,6 +51,17 @@ def run(twitter=True):
                     datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
                 )
             )
+
+    if facebook:
+        for tag in tags:
+            fsp.scrape_fb(
+                token=fb_token,
+                ids=tag,
+                outfile='{}/{}--{}.json'.format(
+                    facebook_dir,
+                    tag,
+                    datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+                )
 
 if __name__ == '__main__':
     run()
