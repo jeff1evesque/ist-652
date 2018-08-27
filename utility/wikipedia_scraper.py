ddaa7dd7ba6efeb5a4ad602653528a5819a6a9cb
jeffrey levesque <jeff1evesque@yahoo.com>
2018-08-26 22:10:45 -0400

    #17: wikipedia_scraper.py, enhance str replace

--- utility/wikipedia_scraper.py
+++ utility/wikipedia_scraper.py
@@ -3,6 +3,7 @@
 import json
 import requests
 import wikipedia
+from functools import reduce
 
 def wikipedia_scraper(date, project='en.wikipedia.org', outfile='facebook.json'):
     '''
@@ -30,14 +31,18 @@ def wikipedia_scraper(date, project='en.wikipedia.org', outfile='facebook.json')
         # write article to textfile
         for item in json.loads(r.text)['items'][0]['articles']:
             article = item['article']
+            repls = {':': '--colon--', '/': '--fslash--'}
+            article_filename = reduce(lambda a, kv: a.replace(*kv), repls.items(), article)
 
             try:
-                with open('data/wikipedia/articles/{}.txt'.format(article.replace(':', '-'), 'w')) as txtfile:
+                with open('data/wikipedia/articles/{}.txt'.format(article_filename)) as txtfile:
                     txtfile.write(wikipedia.WikipediaPage(title=article).summary)
             except OSError as e:
                 print('not a valid article: {}'.format(e))
             except wikipedia.exceptions.DisambiguationError as e:
                 print('{} not valid, alternative titles: {}'.format(article, e.options))
+            except KeyError as e:
+                print('{} not valid: {}'.format(article, e))
 
         # report top 1000 article
         json.dump(r.json(), jsonfile, indent=4)
