6bd47e9dbc1d9fda2f67906f56b1b92ca82c6667
jeffrey levesque <jeff1evesque@yahoo.com>
2018-08-26 21:16:57 -0400

    #17: run.py, remove 'frequency_counter'

--- run.py
+++ run.py
@@ -6,10 +6,9 @@ from config import hashtags
 from sys import argv
 from utility.twitter_scraper import twitter_scraper
 from utility.wikipedia_scraper import wikipedia_scraper
-from utility.frequency_counter import frequency_counter
 from dateutil.relativedelta import relativedelta
 
-def run(twitter=True, wikipedia=True):
+def run(twitter=False, wikipedia=True):
     '''
 
     execute custom twitter + wikipedia scraping.
