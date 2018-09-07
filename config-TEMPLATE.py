2739c6dc23d6a217181822f4224e59fb7b95f52c
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-06 20:32:47 -0400

    #21: config-TEMPLATE.py, create file

---
+++ config-TEMPLATE.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+endpoint='xxx.xxx.xxx.xxx'
+token='xxxxxxxxxxxxxxxxxx'
+
+hashtags = [
+    'apple',
+    'walmart',
+    'amazon',
+    'democrats',
+    'gop',
+    'nfl',
+    'nba',
+    'mlb',
+    'nhl',
+    'shakira',
+    'eminem',
+    'rihanna',
+    'justin_bieber',
+    'vin_diesel',
+    'will_smith',
+    'dwayne_johnson',
+    'jason_stratham',
+]
