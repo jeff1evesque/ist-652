581dc53cced0fe4f0c18336895584653f5bf95f0
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-12 22:49:01 -0400

    #21: run.py, use 'endpoint' from 'config'

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from utility.twitter_scraper import twitter_scraper
 from utility.wikipedia_scraper import wikipedia_scraper
 from dateutil.relativedelta import relativedelta
 
-def run(twitter=True, wikipedia=True, endpoint=False):
+def run(twitter=True, wikipedia=True):
     '''
 
     execute custom twitter + wikipedia scraping.
