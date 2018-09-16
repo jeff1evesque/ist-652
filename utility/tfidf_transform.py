c25600d0a5fa81b283292a094f9f3f6954fbcf8c
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-16 17:05:59 -0400

    #31: tfidf_tranform.py, add file

---
+++ utility/tfidf_transform.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tfidf_transform(corpus, outfile='tfidf.json'):
+
+    '''
+
+    This file requires sklearn's tokenizer:
+
+        - http://scikit-learn.org/stable/modules/feature_extraction.html
+
+    '''
+
+    with open(outfile, 'w') as jsonfile:
+        # initialize tokenizer
+        myTfidf = TfidfVectorizer()
+
+        # calculate tf-idf for each word
+        myTfs = myTfidf.fit_transform(corpus.values())
+
+        # report top 1000 article
+        json.dump(myTfs, jsonfile, indent=4)
+
+if __name__ == '__main__':
+    tdif_transform(*argv[1:])
