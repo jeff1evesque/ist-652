9073d1b378aa9019d51760d980b064968a5995a7
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-20 21:05:52 -0400

    #38: reduce_features.py, add file

---
+++ utility/reduce_features.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
+def chi2(X, y, k=5):
+    '''
+
+    feature selection using chi-squared.
+
+    '''
+
+    X_new = SelectKBest(chi2, k=k).fit_transform(X, y)
+    y_new = X.columns.values[selector.get_support()]
+
+    return({'features': X_new, 'labels': y_new})
