3e0c0ad978001299ef1c0f8dcae4e4101c09679f
jeffrey levesque <jeff1evesque@yahoo.com>
2018-09-20 23:36:22 -0400

    #38: reduce_features.py, return only 'X'

--- utility/reduce_features.py
+++ utility/reduce_features.py
@@ -6,12 +6,11 @@ from sklearn.feature_selection import chi2
 def chi_squared(X, y, k=5):
     '''
 
-    feature selection using chi-squared.
+    feature reduction using chi-squared.
 
     '''
 
     ch2 = SelectKBest(chi2, k=k)
     X_new = ch2.fit_transform(X, y)
-    y_new = [feature_names[i] for i in ch2.get_support(indices=True)]
 
-    return({'features': X_new, 'labels': y_new})
+    return(X_new)
